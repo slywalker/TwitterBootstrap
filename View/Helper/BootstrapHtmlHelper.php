fc5da0e2c38e70d3f33f8a2b501534cdc39e92c3
Yasuo Harada <slywalker.net@gmail.com>
2012-02-07 21:02:51 +0900

    merge master

--- View/Helper/BootstrapHtmlHelper.php
+++ View/Helper/BootstrapHtmlHelper.php
@@ -1,25 +1,45 @@
 <?php
-App::uses('AppHelper', 'View/Helper');
+App::uses('HtmlHelper', 'View/Helper');
 App::uses('Inflector', 'Utility');
 
-class BootstrapHtmlHelper extends AppHelper {
+class BootstrapHtmlHelper extends HtmlHelper {
 
 	public $helpers = array('Html');
 
-	public function css($url = 'bootstrap.min.css', $rel = null, $options = array()) {
+	public function css($url = null, $rel = null, $options = array()) {
+		if (empty($url)) {
+			$url = 'bootstrap.min.css';
+			$pluginRoot = dirname(dirname(__DIR__));
+			$pluginName = end(explode(DS, $pluginRoot));
+			$url = '/' . Inflector::underscore($pluginName) . '/css/' . $url;
+		}
+		return parent::css($url, $rel, $options);
+	}
+
+	public function bootstrapCss($url = 'bootstrap.min.css', $rel = null, $options = array()) {
 		$pluginRoot = dirname(dirname(__DIR__));
 		$pluginName = end(explode(DS, $pluginRoot));
 
 		$url = '/' . Inflector::underscore($pluginName) . '/css/' . $url;
-		return $this->Html->css($url, $rel, $options);
+		return parent::css($url, $rel, $options);
 	}
 
-	public function script($url = 'bootstrap.min.js', $options = array()) {
+	public function script($url = null, $options = array()) {
+		if (empty($url)) {
+			$url = 'bootstrap.min.js';
+			$pluginRoot = dirname(dirname(__DIR__));
+			$pluginName = end(explode(DS, $pluginRoot));
+			$url = '/' . Inflector::underscore($pluginName) . '/js/' . $url;
+		}
+		return parent::script($url, $options);
+	}
+
+	public function bootstrapScript($url = 'bootstrap.min.js', $options = array()) {
 		$pluginRoot = dirname(dirname(__DIR__));
 		$pluginName = end(explode(DS, $pluginRoot));
 
 		$url = '/' . Inflector::underscore($pluginName) . '/js/' . $url;
-		return $this->Html->script($url, $options);
+		return parent::script($url, $options);
 	}
 
 	public function breadcrumb($items, $options = array()) {
@@ -33,10 +53,10 @@ class BootstrapHtmlHelper extends AppHelper {
 		for ($i=0; $i < $count - 1; $i++) {
 			$text = $items[$i];
 			$text .= '&nbsp;<span class="divider">/</span>';
-			$li[] = $this->Html->tag('li', $text);
+			$li[] = parent::tag('li', $text);
 		}
-		$li[] = $this->Html->tag('li', end($items), array('class' => 'active'));
-		return $this->Html->tag('ul', implode("\n", $li), $options);
+		$li[] = parent::tag('li', end($items), array('class' => 'active'));
+		return parent::tag('ul', implode("\n", $li), $options);
 	}
 
 }
