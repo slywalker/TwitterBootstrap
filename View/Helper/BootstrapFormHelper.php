763b44126ee864a35796c5d3ecd9fe3956aea50e
Alexandro Luiz Dias <ali3nado@gmail.com>
2013-02-16 16:25:59 -0200

    Update View/Helper/BootstrapFormHelper.php
    Added method dropdown from buttons.

--- View/Helper/BootstrapFormHelper.php
+++ View/Helper/BootstrapFormHelper.php
@@ -38,6 +38,31 @@ class BootstrapFormHelper extends FormHelper {
 			return $this->Html->tag('span', $options['value'], $options['class']);
 		}
 	}
+	
+	public function dropdown($title = 'DropText', $links = array(), $options = array()) {
+        
+	        $links      = isset($links) ? $links : null;
+	        $size       = isset($options['size']) ? $options['size'] : null;
+	        $direction  = isset($options['direct']) ? $options['direct'] : 'dropdown';
+	        $class      = isset($options['class']) ? $options['class'] : null;
+	        
+	        $content    = $this->Html->tag('button', $title, array('class' => self::CLASS_BUTTON . ' ' . $size)); 
+	        $content    .= $this->Html->tag('button', $this->Html->tag('span',null,array('class' => 'caret')), array('class' => self::CLASS_BUTTON . ' ' . $size . ' dropdown-toggle ', 'data-toggle' => $direction)); 
+	         
+	        if(!is_null($links)){
+	            $menu_links = null;
+	            foreach ($links as $link){
+	                list($title, $url, $options) = $link;
+	                $menu_links .= $this->Html->tag('li', $this->Html->link($title, $url, $options));
+	            }
+	            if(!is_null($menu_links)){
+	                $content .= $this->Html->tag('ul', $menu_links, array('class' => 'dropdown-menu'));
+	            }
+	        }
+	        
+	        return $this->Html->tag('div', $content, array('class' => self::CLASS_BUTTON_GROUP . ' ' . $direction . ' ' . $class));
+
+    	}
 
 	public function addon($fieldName, $options = array(), $before = false) {
 		if ($before) {
