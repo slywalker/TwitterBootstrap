cc907c6dfbc437042c0d76a259945f72621ed6cb
Angel S. Moreno <angelxmoreno@gmail.com>
2013-06-02 03:41:40 -0400

    Added windows compatible commands

--- Console/Command/MakeShell.php
+++ Console/Command/MakeShell.php
@@ -10,11 +10,17 @@ class MakeShell extends TwitterBootstrapAppShell {
 	public function main() {
 		$bootstrapLess = WWW_ROOT . self::BOOTSTRAP_LESS;
 		$responsiveLess = WWW_ROOT . self::RESPONSIVE_LESS;
+		$isWindows = strpos(PHP_OS, 'WIN');
 
 		$command = "cd {$this->bootstrapPath} && ";
 		$command .= "make bootstrap BOOTSTRAP_LESS='{$bootstrapLess}' BOOTSTRAP_RESPONSIVE_LESS='{$responsiveLess}' && ";
-		$command .= "cp -R bootstrap/* " . WWW_ROOT . " && ";
-		$command .= "rm -rf bootstrap";
+		if ($isWindows) {
+			$command .= "xcopy /s /e /y bootstrap/* " . WWW_ROOT . " && ";
+			$command .= "rmdir /s /q bootstrap";
+		} else {
+			$command .= "cp -R bootstrap/* " . WWW_ROOT . " && ";
+			$command .= "rm -rf bootstrap";
+		}
 
 		exec($command, $output);
 		$this->out($output);
